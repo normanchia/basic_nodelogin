02a34c9af37739af51af7e1fcf6d28299ff35669
Norman Chia <norman.chia.minjie@gmail.com>
2023-11-15 00:50:18 +0800

    use selenium

--- test/ui/loginForm.test.js
+++ test/ui/loginForm.test.js
@@ -1,20 +1,26 @@
-// UI Test: Login Form Interaction
-
-const puppeteer = require("puppeteer");
+const { Builder, By } = require("selenium-webdriver");
 
 describe("Login Form", () => {
   it("should allow a user to submit login credentials", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000"); // Your app's URL
+    // Initialize the Selenium WebDriver
+    let driver = await new Builder().forBrowser("chrome").build();
 
-    await page.type("input[name=username]", "testUser");
-    await page.type("input[name=password]", "testPass");
-    await page.click("button[type=submit]");
+    try {
+      // Navigate to the application's URL
+      await driver.get("http://localhost:3000");
 
-    // Add assertions to check for successful login
-    // For example, checking if the user is redirected to the home page
+      // Enter username and password
+      await driver.findElement(By.name("username")).sendKeys("testUser");
+      await driver.findElement(By.name("password")).sendKeys("testPass");
 
-    await browser.close();
+      // Click the submit button
+      await driver.findElement(By.css("button[type=submit]")).click();
+
+      // Add assertions here to check for successful login
+      // For example, you could check the URL or look for a specific element on the page
+    } finally {
+      // Close the browser
+      await driver.quit();
+    }
   });
 });
